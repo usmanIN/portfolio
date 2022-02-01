38bcd0553947e91e0df5956dab466c32479f185a
usmanIN <61617956+usmanIN@users.noreply.github.com>
2022-02-01 13:18:01 +0530

    added content

--- assessts/script.js
+++ assessts/script.js
@@ -5,15 +5,18 @@ if(window.innerWidth >=768){
       var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
       var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       var scrolled = (winScroll / height) * 100;
-      document.getElementById("bar").style.backgroundColor ="#04AA6D";
+
+      document.getElementById("bar").style.backgroundColor ="#222222";
+
       let array =  this.document.getElementById("menu").querySelectorAll("a");
 
-      array[0].style.color = !(scrolled<=40)?"#FFFFFF":"#000000";
-      array[1].style.color = !(scrolled<=50)?"#FFFFFF":"#000000";
-      array[2].style.color = !(scrolled<=55)?"#FFFFFF":"#000000";
-      array[3].style.color = !(scrolled<=60)?"#FFFFFF":"#000000";
-      array[4].style.color = !(scrolled<=65)?"#FFFFFF":"#000000";
+      for (let index = 0; index < array.length; index++) {
+        
+        position = ((array[index].getBoundingClientRect().right / window.innerWidth) * 100) - 2;
+        array[index].style.color = !(scrolled<=position)?"#FFFFFF":"#000000";  
 
+      }
+      
       document.getElementById("menu").style.color ="#FFF";
       document.getElementById("bar").style.width = scrolled + "%";
 
